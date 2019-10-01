e60f6ef41bb6087859396c10968a58b40c7b1d3d
CasperLC <43999061+CasperLC@users.noreply.github.com>
2019-10-01 10:40:56 +0200

    Initial commit

---
+++ MovieRatingLibrary/Entities/MovieReview.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieRatingLibrary.Entities
+{
+    public class MovieReview
+    {
+        public int ReviewerId { get; set; }
+        public int MovieId { get; set; }
+        public int Grade { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ MovieRatingLibrary/MovieRating.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace MovieRatingLibrary
+{
+    public class MovieRating: IMovieRating
+    {
+        public List<object> AllReviewsFromReviewer(int reviewerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double AverageRatingFromReviewer(int reviewerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int TimesReviewerHasGivenGrade(int reviewerId, int grade)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int HowManyReviewsOfMovie(int movieId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int AverageRatingMovieReceived(int movieId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int TimesMovieReceivedGrade(int movieId, int grade)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int MovieWithMostTopRates()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int ReviewerWithMostReviews()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<int> TopNofMovies(int grade)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<int> MoviesReviewedByN(int reviewerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<int> ReviewersThatReviewedMovie(int movieId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
